e7352692ad7f092c0549481dc122f612436e891f
Aiyualive <liuaiyu1302@gmail.com>
2023-09-23 22:21:15 +0200

    add json utils

--- app/utils/general.ts
+++ app/utils/general.ts
@@ -14,3 +14,33 @@ export function combineDateAndTime(date: Date, time: Date) {
 
     return new Date(year, month, day, hours, minutes, seconds);
 }
+
+export function validateTraits(input): boolean {
+    // Validate if the parsed value is an array
+    if (Array.isArray(input)) {
+        // Check if each item in the array has the required properties
+        const isValid = input.every((traitObject) =>
+            Object.prototype.hasOwnProperty.call(traitObject, 'trait_type') &&
+            Object.prototype.hasOwnProperty.call(traitObject, 'value')
+        );
+
+        if (!isValid) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+export function addExtension(originalString, newExtension) {
+    const parts = originalString.split('.');
+
+    if (parts.length >= 2) {
+        // Remove the existing extension and add the new one
+        parts.pop();
+    }
+
+    parts.push(newExtension);
+
+    return parts.join('.');
+}
