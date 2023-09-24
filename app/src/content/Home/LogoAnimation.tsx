cfe3a19c1156d91a7c6fc3b32c25b1e4b3e283e1
Aiyualive <liuaiyu1302@gmail.com>
2023-09-24 18:43:04 +0200

    savepoint

--- app/src/content/Home/LogoAnimation.tsx
+++ app/src/content/Home/LogoAnimation.tsx
@@ -2,8 +2,12 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 export function LogoAnimation() {
+    const isMobile = useMediaQuery(useTheme().breakpoints.down('sm'));
+    const mobileFactor = isMobile ? 0.8 : 1;
     const container = {
         hidden: { opacity: 0 },
         show: {
@@ -98,12 +102,12 @@ export function LogoAnimation() {
             hidden: {
                 opacity: 0,
                 x: 30 + offset,
-                y: -200
+                // y: -200
             },
             show: {
                 opacity: 1,
                 x: -70 + offset,
-                y: 0,
+                // y: 0,
                 transition: {
                     duration: 1,
                     ease: [0.02, 0.6, 0.01, 0.91]
@@ -114,37 +118,43 @@ export function LogoAnimation() {
 
 
     const AR = 125/48;
-    const size = 50;
-
+    const sizeE = 50 * mobileFactor;
     const eLogo = <Image
         src={"/logos/animation/e.png"}
         alt={"logo"}
-        height={size}
-        width={size * AR}
+        height={sizeE}
+        width={sizeE * AR}
     />;
 
     const ARp1 = 136/175;
+    const sizeP1 = 168 * mobileFactor;
     const p1Logo = <Image
         src={"/logos/animation/p1.png"}
         alt={"logo"}
-        height={168}
-        width={168 * ARp1}
+        height={sizeP1}
+        width={sizeP1 * ARp1}
     />;
 
     const ARp2 = 97/110;
+    const sizeP2 = 112 * mobileFactor;
     const p2Logo = <Image
         src={"/logos/animation/p2.png"}
         alt={"logo"}
-        height={112}
-        width={112 * ARp2}
+        height={sizeP2}
+        width={sizeP2 * ARp2}
     />;
 
+    const offset1 = isMobile ? 80 : 0;
+    const offset2 = isMobile ? 40 : -40;
+
+    console.log("offser", offset1, offset2);
     return (
         <Box
             component={"div"}
+            className={"sm:mt-[100px] mt-[50px]"}
+            // marginTop={{sm: "50px", md: "100px"}}
             sx={{
-                left: 75,
-                marginTop: {sm: "50px", md: "100px"},
+                // left: 75,
                 width: "100%",
                 position: "relative",
                 display: "flex",
@@ -153,6 +163,8 @@ export function LogoAnimation() {
             }}
         >
             {/* E part */}
+            {/*<div className="absolute ">*/}
+            {/*    <div className={"flex flex-row"}>*/}
             <motion.div
                 variants={container}
                 initial="hidden"
@@ -170,28 +182,20 @@ export function LogoAnimation() {
             </motion.div>
 
             {/* P part */}
-            {/*<div*/}
-            {/*    style={{*/}
-            {/*        width: "100%",*/}
-            {/*        // position: "relative",*/}
-            {/*        display: "flex",*/}
-            {/*        flexDirection: "row",*/}
-            {/*        // alignItems: "center",*/}
-            {/*    }}*/}
-            {/*>*/}
             <motion.div
                 variants={container}
                 initial="hidden"
                 animate="show"
                 className="flex flex-row"
             >
-                <motion.div variants={itemY2(0)}>
+                <motion.div variants={itemY2(offset1)}>
                     {p1Logo}
                 </motion.div>
-                <motion.div variants={itemY2(-40)}>
+                <motion.div variants={itemY2(offset2)}>
                     {p2Logo}
                 </motion.div>
             </motion.div>
+            {/*    </div>*/}
             {/*</div>*/}
         </Box>
     );
