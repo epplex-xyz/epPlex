0677bc8937042047d6eeb90b73fef06553413273
Aiyualive <liuaiyu1302@gmail.com>
2024-01-20 22:59:50 +0100

    fix: delete

--- programs/epplex-core/src/state/mod.rs
+++ programs/epplex-core/src/state/mod.rs
@@ -4,9 +4,5 @@ pub use program_delegate::*;
 pub mod collection_config;
 pub use collection_config::*;
 
-pub mod mint22;
-pub use mint22::*;
-
-
 pub mod global_collection_config;
 pub use global_collection_config::*;
