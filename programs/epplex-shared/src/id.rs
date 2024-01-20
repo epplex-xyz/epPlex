f60711c065111979665132984d1679dd97adc89a
Aiyualive <liuaiyu1302@gmail.com>
2024-01-20 18:03:44 +0100

    fix: update id

---
+++ programs/epplex-shared/src/id.rs
@@ -0,0 +1,3 @@
+use anchor_lang::prelude::*;
+
+declare_id!("epShtSs6bXafJMhjKCJM8CLyZJSDvzVGwfmEUiXTgqJ");
--- programs/epplex-shared/src/lib.rs
+++ programs/epplex-shared/src/lib.rs
@@ -5,9 +5,9 @@ pub use state::*;
 
 pub mod program_wrappers;
 pub use program_wrappers::*;
+pub mod id;
+pub use id::ID;
 
-
-declare_id!("Cxtfv4jRc6H7Yb8gVpdt7tTFte8RWEa8RgMk61qhRdiP");
 
 #[program]
 pub mod epplex_shared {}
