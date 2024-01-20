58ad190b913e6f31a742456d7da270ba8a8a58ff
Aiyualive <liuaiyu1302@gmail.com>
2024-01-20 18:04:16 +0100

    fix: rename

--- programs/epplex-metadata/src/lib.rs
+++ programs/epplex-metadata/src/lib.rs
@@ -9,13 +9,12 @@ pub use id::ID;
 pub mod actions;
 pub use actions::*;
 
-
 #[program]
 pub mod epplex_metadata {
     use super::*;
 
     #[access_control(ctx.accounts.validate(&ctx, &params))]
-    pub fn create_metadata(ctx: Context<CreateMetadata>,  params: CreateMetadataParams) -> Result<()> {
-        CreateMetadata::actuate(ctx, params)
+    pub fn metadata_create(ctx: Context<MetadataCreate>, params: MetadataCreateParams) -> Result<()> {
+        MetadataCreate::actuate(ctx, params)
     }
 }
