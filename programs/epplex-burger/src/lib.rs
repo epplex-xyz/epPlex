0e8fb20c120417da87a50aac01ba7dcc4d9ea276
Aiyualive <liuaiyu1302@gmail.com>
2024-01-21 02:35:17 +0100

    fix: overall refactor

--- programs/epplex-burger/src/lib.rs
+++ programs/epplex-burger/src/lib.rs
@@ -12,12 +12,17 @@ pub use id::ID;
 
 pub mod token_utils;
 pub use token_utils::*;
-
 use anchor_lang::prelude::*;
 use anchor_spl::{
-    token_interface::{Mint, TokenAccount}
+    token::{Mint, TokenAccount},
+    token_2022::{self},
+    token_interface::{
+        Mint as MintInterface,
+        TokenAccount as TokenAccountInterface
+    }
 };
 use spl_token_metadata_interface::state::TokenMetadata;
+use epplex_shared::Token2022;
 
 #[program]
 pub mod epplex_burger {
@@ -35,7 +40,7 @@ pub mod epplex_burger {
     }
 
     #[access_control(ctx.accounts.validate(&ctx, &params))]
-    pub fn token_renew(ctx: Context<TokenRenew>, params: &TokenRenewParams) -> Result<()> {
+    pub fn token_renew(ctx: Context<TokenRenew>, params: TokenRenewParams) -> Result<()> {
         TokenRenew::actuate(ctx, params)
     }
 
--- programs/epplex-burger/src/state/ephemeral_state.rs
+++ programs/epplex-burger/src/state/ephemeral_state.rs
@@ -1,9 +1,23 @@
+use anchor_lang::solana_program::pubkey;
+use anchor_lang::prelude::*;
+
 pub const SEED_TOKEN_METADATA: &[u8] = b"metadata";
 
+pub const SOL: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
+pub const SUPPORTED_TOKENS: [Pubkey;1] = [
+    SOL
+];
+
+
+// Fields
 pub const EXPIRY_FIELD: &str = "expirationDate";
 pub const RENEWAL_FIELD: &str = "renewalAmount";
 pub const FOR_SALE_FIELD: &str = "forSale";
 pub const PRICE_FIELD: &str = "price";
 
 
+
+// Prolly these need to be stored in some kind of config
+// TIME
 pub const ONE_WEEK: i64 = 604800;
+pub const ONE_DAY: i64 = 86400;
