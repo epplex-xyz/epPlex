9a30c29a621b11ce0e2a07dc09a3897cfb99ca33
Aiyualive <liuaiyu1302@gmail.com>
2024-02-12 02:00:31 +0100

    feat: use universal admin

--- programs/epplex-burger/src/state/admin.rs
+++ programs/epplex-burger/src/state/admin.rs
@@ -2,4 +2,4 @@ use anchor_lang::solana_program::pubkey;
 use anchor_lang::prelude::*;
 
 // Payer keypair has to sign everything
-pub const ADMIN_PUBKEY: Pubkey = pubkey!("BuHRzpGi4t9ho8rtBNCKCRrPE26EG2CGsq3YiVCkhXr7");
+pub const ADMIN_PUBKEY: Pubkey = pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr5cuzh");
