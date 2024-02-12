4e69e3112a8366cce1d4e24257cf58214777ec24
Aiyualive <liuaiyu1302@gmail.com>
2024-02-12 01:49:21 +0100

    feat: update to u128

--- programs/epplex-core/src/state/global_collection_config.rs
+++ programs/epplex-core/src/state/global_collection_config.rs
@@ -6,12 +6,12 @@ pub const SEED_GLOBAL_COLLECTION_CONFIG: &[u8] = b"GLOBAL_COLLECTION";
 
 #[account]
 pub struct GlobalCollectionConfig {
-    pub collection_counter: u64,
+    pub collection_counter: u128,
     pub bump: u8
 }
 
 impl GlobalCollectionConfig {
     pub const LEN: usize = DISCRIMINATOR_LENGTH
-    + BITS_64
+    + (BITS_64 * 2)
     + BITS_8;
 }
