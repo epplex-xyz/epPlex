774836680fee3ae5c2299a5821a82c55681cb326
Aiyualive <liuaiyu1302@gmail.com>
2024-02-13 07:26:02 +0100

    feat: default to local keys, add balance checks

--- programs/epplex-core/src/id.rs
+++ programs/epplex-core/src/id.rs
@@ -1,3 +1,4 @@
 use anchor_lang::prelude::*;
 
-declare_id!("epCoD6BqcNinLvKN3KkY55vk4Kxs3W1JTENs1xqWUTg");
+// declare_id!("epCoD6BqcNinLvKN3KkY55vk4Kxs3W1JTENs1xqWUTg");
+declare_id!("LepCn3tW66Fh7CGsJ7qjQaontU7SvEoURnxkEY78j1j");
--- programs/epplex-shared/src/id.rs
+++ programs/epplex-shared/src/id.rs
@@ -1,3 +1,4 @@
 use anchor_lang::prelude::*;
 
-declare_id!("epShtSs6bXafJMhjKCJM8CLyZJSDvzVGwfmEUiXTgqJ");
+// declare_id!("epShtSs6bXafJMhjKCJM8CLyZJSDvzVGwfmEUiXTgqJ");
+declare_id!("LepS8gH3rVDRdAy5X9xGE8VcnGnFKmkpfm21Paiqq3i");
