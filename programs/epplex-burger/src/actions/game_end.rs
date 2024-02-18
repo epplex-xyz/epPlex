2237bd0b3de7128cd85630d5449d009ed5f31197
Jimii <jimmyimpulse2@gmail.com>
2024-02-18 13:30:03 +0300

    feat: check md fields when ending a game

--- programs/epplex-burger/src/actions/game_end.rs
+++ programs/epplex-burger/src/actions/game_end.rs
@@ -1,4 +1,7 @@
+use crate::*;
+
 use anchor_lang::prelude::*;
+use epplex_shared::Token2022;
 
 use crate::{GameConfig, GameStatus, SEED_GAME_CONFIG};
 
@@ -14,14 +17,26 @@ pub struct GameEnd<'info> {
     )]
     pub game_config: Account<'info, GameConfig>,
 
+    #[account(
+        mut,
+        mint::token_program = token22_program.key(), // ? is this check necessary
+        constraint = mint.decimals == 0,
+        constraint = mint.supply == 1,
+    )]
+    pub mint: Box<InterfaceAccount<'info, MintInterface>>,
+
+    pub token22_program: Program<'info, Token2022>,
     pub system_program: Program<'info, System>,
 }
 
 impl GameEnd<'_> {
-    pub fn validate(&self, _ctx: &Context<Self>) -> Result<()> {
+    pub fn validate(&self, ctx: &Context<Self>) -> Result<()> {
         self.game_config.check_phase_end_ts()?;
 
-        // ! make sure that the metadata fields are populated 
+        // ! make sure that the metadata fields are populated.
+        // ! meaning the account participated in the game
+        self.game_config
+            .check_metadata_fields_filled(&ctx.accounts.mint.to_account_info())?;
 
         Ok(())
     }
