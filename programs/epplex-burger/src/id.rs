88e49d2c8d5ad03ccb036c5c04cdf87c2610c75b
Aiyualive <liuaiyu1302@gmail.com>
2024-02-21 14:22:18 +0100

    refactor: remove the commented out id

--- programs/epplex-burger/src/id.rs
+++ programs/epplex-burger/src/id.rs
@@ -1,4 +1,3 @@
 use anchor_lang::prelude::*;
 
-// declare_id!("epBuJysRKuFMMWTWoX6ZKPz5WTZWb98mDqn1emVj84n");
 declare_id!("LepByYNXCXLAQicahdRvvxBD45SMNHJgoNsAUDLyG1N");
--- programs/epplex-core/src/id.rs
+++ programs/epplex-core/src/id.rs
@@ -1,4 +1,3 @@
 use anchor_lang::prelude::*;
 
-// declare_id!("epCoD6BqcNinLvKN3KkY55vk4Kxs3W1JTENs1xqWUTg");
 declare_id!("LepCn3tW66Fh7CGsJ7qjQaontU7SvEoURnxkEY78j1j");
