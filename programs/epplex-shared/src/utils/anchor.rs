51520fbacbd9683e4b832cab25282fd39ba7a9b4
Aiyualive <liuaiyu1302@gmail.com>
2024-03-01 04:34:06 +0400

    feat: add util to close account

---
+++ programs/epplex-shared/src/utils/anchor.rs
@@ -0,0 +1,13 @@
+// https://solana.stackexchange.com/questions/11606/how-to-close-a-pda-account-when-the-structure-has-been-changed
+pub fn close<'info>(info: AccountInfo<'info>, sol_destination: AccountInfo<'info>) -> Result<()> {
+    // Transfer tokens from the account to the sol_destination.
+    let dest_starting_lamports = sol_destination.lamports();
+    **sol_destination.lamports.borrow_mut() =
+        dest_starting_lamports.checked_add(info.lamports()).unwrap();
+    **info.lamports.borrow_mut() = 0;
+
+    info.assign(&system_program::ID);
+    info.realloc(0, false).map_err(Into::into)
+    // info.realloc(0, false).map_err(Into::<anchor_lang::prelude::ProgramError>::into);
+
+}
