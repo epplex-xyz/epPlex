62b2cb7ed2f8d555ed1efbcb9d49bf438baeeb40
Aiyualive <liuaiyu1302@gmail.com>
2024-03-13 17:36:02 +0100

    feat: update tokengameburn

--- programs/epplex-burger/src/events.rs
+++ programs/epplex-burger/src/events.rs
@@ -4,12 +4,16 @@ use crate::*;
 pub struct EvTokenGameVote {
     pub answer: String,
     pub participant: Pubkey,
+    pub game_round_id: u8,
+    pub nft: Pubkey,
 }
 
 #[event]
 pub struct EvTokenGameBurn {
-    pub answer: String,
+    pub nft: Pubkey,
+    pub game_round_id: u8,
     pub participant: Pubkey,
+    pub burn_timestamp: i64,
 }
 
 #[event]
