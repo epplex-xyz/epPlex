44e890ed344c1afbb443d8f7a793b9801a709551
Aiyualive <liuaiyu1302@gmail.com>
2024-03-14 19:09:50 +0100

    refactor: move utils out

--- programs/epplex-burger/src/utils/token_utils.rs
+++ programs/epplex-burger/src/utils/token_utils.rs
@@ -1,35 +1,14 @@
 use crate::*;
 use anchor_lang::solana_program;
+use solana_program::program_pack::Pack;
 
-// pub fn update_token_metadata<'info>(
-//     program_id: &Pubkey,
-//     metadata: &AccountInfo<'info>,
-//     update_authority: &AccountInfo<'info>,
-//     field: spl_token_metadata_interface::state::Field,
-//     value: String,
-// ) -> Result<()> {
-//     let ix = spl_token_metadata_interface::instruction::update_field(
-//         &program_id,
-//         &metadata.key(),
-//         &update_authority.key(),
-//         field,
-//         value
-//     );
-//
-//     let account_infos: Vec<AccountInfo> = vec![
-//         metadata.to_account_info(),
-//         update_authority.to_account_info(),
-//     ];
-//
-//     solana_program::program::invoke(
-//         &ix,
-//         &account_infos[..],
-//     )?;
-//
-//     Ok(())
-// }
-//
+pub fn get_token_account_owner(token_account: &AccountInfo) -> Result<Pubkey> {
+    let state =
+        spl_token_2022::state::Account::unpack_from_slice(&token_account.try_borrow_data()?)?;
+    Ok(state.owner)
+}
 
+// prolly could use the shared function
 pub fn update_token_metadata_signed<'info>(
     program_id: &Pubkey,
     metadata: &AccountInfo<'info>,
@@ -97,87 +76,3 @@ pub fn transfer_token_with_pda<'info>(
 
     Ok(())
 }
-
-pub fn burn_token<'info>(
-    mint_account: &AccountInfo<'info>,
-    token_account: &AccountInfo<'info>,
-    program: Pubkey,
-    authority: &AccountInfo<'info>,
-) -> Result<()> {
-    let ix = spl_token_2022::instruction::burn(
-        &program,
-        &token_account.key(),
-        &mint_account.key(),
-        &authority.key(),
-        &[],
-        1,
-    )?;
-
-    let account_infos: Vec<AccountInfo> = vec![
-        token_account.to_account_info(),
-        mint_account.to_account_info(),
-        authority.to_account_info(),
-    ];
-
-    // TODO not ideal
-    let (_, bump) = Pubkey::find_program_address(&[SEED_PROGRAM_DELEGATE], &ID);
-
-    let seeds = &[SEED_PROGRAM_DELEGATE, &[bump]];
-    solana_program::program::invoke_signed(&ix, &account_infos[..], &[&seeds[..]])?;
-
-    Ok(())
-}
-
-pub fn close_mint<'info>(
-    program: Pubkey,
-    token_account: &AccountInfo<'info>,
-    destination_account: &AccountInfo<'info>,
-    owner: &AccountInfo<'info>,
-) -> Result<()> {
-    let ix = spl_token_2022::instruction::close_account(
-        &program,
-        &token_account.key(),
-        &destination_account.key(),
-        &owner.key(),
-        &[],
-    )?;
-
-    let account_infos: Vec<AccountInfo> = vec![
-        token_account.to_account_info(),
-        destination_account.to_account_info(),
-        owner.to_account_info(),
-    ];
-
-    // TODO not ideal
-    let (_, bump) = Pubkey::find_program_address(&[SEED_PROGRAM_DELEGATE], &ID);
-
-    let seeds = &[SEED_PROGRAM_DELEGATE, &[bump]];
-    solana_program::program::invoke_signed(&ix, &account_infos[..], &[&seeds[..]])?;
-
-    Ok(())
-}
-
-// Anchor transfer method
-// pub fn transfer_token<'info>(
-//     amount: u64,
-//     decimals: u8,
-//     authority: &Signer<'info>,
-//     mint: &AccountInfo<'info>,
-//     token_owner_account: &Account<'info, TokenAccount>,
-//     token_vault: &Account<'info, TokenAccount>,
-//     token_program: &AccountInfo<'info>,
-// ) -> Result<()> {
-//     token_2022::transfer_checked(
-//         CpiContext::new(
-//             token_program.to_account_info(),
-//             token_2022::TransferChecked {
-//                 from: token_owner_account.to_account_info(),
-//                 to: token_vault.to_account_info(),
-//                 mint: mint.to_account_info(),
-//                 authority: authority.to_account_info(),
-//             },
-//         ),
-//         amount,
-//         decimals
-//     )
-// }
--- programs/epplex-shared/src/utils/token_utils.rs
+++ programs/epplex-shared/src/utils/token_utils.rs
@@ -50,3 +50,62 @@ pub fn update_token_metadata_signed<'info>(
 
     Ok(())
 }
+
+pub fn burn_token<'info>(
+    mint_account: &AccountInfo<'info>,
+    token_account: &AccountInfo<'info>,
+    program: Pubkey,
+    authority: &AccountInfo<'info>,
+    seeds: Option<&[&[u8]; 2]>,
+) -> Result<()> {
+    let ix = spl_token_2022::instruction::burn(
+        &program,
+        &token_account.key(),
+        &mint_account.key(),
+        &authority.key(),
+        &[],
+        1,
+    )?;
+
+    let account_infos: Vec<AccountInfo> = vec![
+        token_account.to_account_info(),
+        mint_account.to_account_info(),
+        authority.to_account_info(),
+    ];
+
+    match seeds {
+        Some(s) => solana_program::program::invoke_signed(&ix, &account_infos[..], &[&s[..]])?,
+        None => solana_program::program::invoke(&ix, &account_infos[..])?,
+    }
+
+    Ok(())
+}
+
+pub fn close_mint<'info>(
+    program: Pubkey,
+    token_account: &AccountInfo<'info>,
+    destination_account: &AccountInfo<'info>,
+    owner: &AccountInfo<'info>,
+    seeds: Option<&[&[u8]; 2]>,
+) -> Result<()> {
+    let ix = spl_token_2022::instruction::close_account(
+        &program,
+        &token_account.key(),
+        &destination_account.key(),
+        &owner.key(),
+        &[],
+    )?;
+
+    let account_infos: Vec<AccountInfo> = vec![
+        token_account.to_account_info(),
+        destination_account.to_account_info(),
+        owner.to_account_info(),
+    ];
+
+    match seeds {
+        Some(s) => solana_program::program::invoke_signed(&ix, &account_infos[..], &[&s[..]])?,
+        None => solana_program::program::invoke(&ix, &account_infos[..])?,
+    }
+
+    Ok(())
+}
