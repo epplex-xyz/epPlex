3c7f88cd362577bb4c55c7c2b6e38d9989b600b0
Aiyualive <liuaiyu1302@gmail.com>
2024-03-18 18:14:24 +0100

    remove final close of token account

--- programs/epplex-core/src/actions/membership_burn.rs
+++ programs/epplex-core/src/actions/membership_burn.rs
@@ -5,7 +5,15 @@ use anchor_spl::{
     token_2022::{burn, close_account, Burn, CloseAccount},
     token_interface::TokenAccount as TokenAccountInterface,
 };
-use solana_program::program_pack::Pack;
+
+#[derive(Clone)]
+pub struct WenNewStandard;
+
+impl Id for WenNewStandard {
+    fn id() -> Pubkey {
+        wen_new_standard::ID
+    }
+}
 
 #[derive(Accounts)]
 pub struct MembershipBurn<'info> {
@@ -21,7 +29,8 @@ pub struct MembershipBurn<'info> {
     )]
     pub membership_ata: Box<InterfaceAccount<'info, TokenAccountInterface>>,
 
-    #[account()]
+    // This PDA receives the burn funds
+    #[account(mut)]
     pub burner: Signer<'info>,
 
     #[account(mut)]
@@ -57,6 +66,15 @@ pub struct MembershipBurn<'info> {
     pub epplex_authority: UncheckedAccount<'info>,
 
     pub token22_program: Program<'info, Token2022>,
+
+    #[account(
+        seeds = [wen_new_standard::MANAGER_SEED],
+        seeds::program = wen_new_standard::ID,
+        bump
+    )]
+    pub manager: Option<Account<'info, wen_new_standard::Manager>>,
+
+    pub wns: Option<Program<'info, WenNewStandard>>,
 }
 
 impl MembershipBurn<'_> {
@@ -78,7 +96,35 @@ impl MembershipBurn<'_> {
     /**
      * Closes: ephemeral data, burns token, closes mint, optionally closes ATA
      */
-    pub fn burn(ctx: Context<Self>) -> Result<()> {
+    pub fn burn<'info>(ctx: Context<'_, '_, '_, 'info, MembershipBurn<'info>>) -> Result<()> {
+        // let burner1 = ctx.accounts.burner.to_account_info().clone();
+        // let burner2 = ctx.accounts.burner.to_account_info().clone();
+        // let token_program = ctx.accounts.token22_program.to_account_info().clone();
+
+        // let token_account = ctx.accounts.membership_ata.to_account_info().clone();
+
+        match ctx.accounts.wns {
+            Some(_) => Self::burn_wns(ctx)?,
+            None => Self::burn_standard(ctx)?,
+        }
+
+        // let state =
+        //     spl_token_2022::state::Account::unpack_from_slice(&token_account.try_borrow_data()?)?;
+        // if state.owner == burner1.key() {
+        //     close_account(CpiContext::new(
+        //         token_program,
+        //         CloseAccount {
+        //             account: token_account,
+        //             destination: burner1.clone(),
+        //             authority: burner2,
+        //         },
+        //     ))?;
+        // }
+
+        Ok(())
+    }
+
+    fn burn_standard<'info>(ctx: Context<'_, '_, '_, 'info, MembershipBurn<'info>>) -> Result<()> {
         let seeds: &[&[u8]; 2] = &[SEED_EPHEMERAL_AUTH, &[ctx.bumps.epplex_authority]];
         burn(
             CpiContext::new_with_signer(
@@ -93,6 +139,7 @@ impl MembershipBurn<'_> {
             1,
         )?;
 
+        // Assuming we have epplex_auth is also the closeMintAuth
         close_account(CpiContext::new_with_signer(
             ctx.accounts.token22_program.to_account_info(),
             CloseAccount {
@@ -103,20 +150,27 @@ impl MembershipBurn<'_> {
             &[&seeds[..]],
         ))?;
 
-        // Close ATA if owner of ATA is burner
-        let token_account = ctx.accounts.membership_ata.to_account_info();
-        let state =
-            spl_token_2022::state::Account::unpack_from_slice(&token_account.try_borrow_data()?)?;
-        if state.owner == ctx.accounts.burner.key() {
-            close_account(CpiContext::new(
-                ctx.accounts.token22_program.to_account_info(),
-                CloseAccount {
-                    account: ctx.accounts.membership_ata.to_account_info().clone(),
-                    destination: ctx.accounts.burner.to_account_info().clone(),
-                    authority: ctx.accounts.burner.to_account_info().clone(),
-                },
-            ))?;
-        }
+        Ok(())
+    }
+
+    fn burn_wns<'info>(ctx: Context<'_, '_, '_, 'info, MembershipBurn<'info>>) -> Result<()> {
+        let seeds: &[&[u8]; 2] = &[SEED_EPHEMERAL_AUTH, &[ctx.bumps.epplex_authority]];
+
+        let wns = ctx.accounts.wns.clone().unwrap();
+        let manager = ctx.accounts.manager.clone().unwrap();
+
+        wen_new_standard::cpi::burn_mint_account(CpiContext::new_with_signer(
+            wns.to_account_info(),
+            wen_new_standard::cpi::accounts::BurnMintAccount {
+                payer: ctx.accounts.burner.to_account_info(),
+                user: ctx.accounts.epplex_authority.to_account_info(),
+                mint: ctx.accounts.membership.to_account_info(),
+                mint_token_account: ctx.accounts.membership_ata.to_account_info(),
+                manager: manager.to_account_info(),
+                token_program: ctx.accounts.token22_program.to_account_info(),
+            },
+            &[&seeds[..]],
+        ))?;
 
         Ok(())
     }
