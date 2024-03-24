2d4e15047a66f75608d4c8ff36f3fac846ba1b3d
Aiyualive <liuaiyu1302@gmail.com>
2024-03-24 15:41:37 +0100

    try add admin stuff

--- programs/epplex-shared/src/utils/admin.rs
+++ programs/epplex-shared/src/utils/admin.rs
@@ -4,8 +4,13 @@ use anchor_lang::solana_program::pubkey;
 // Payer keypair has to sign everything
 pub const ADMIN_PUBKEY: Pubkey = pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr5cuzh");
 // pub const ADMIN_PUBKEY: Pubkey = pubkey!("LAdmTEtom7qm3ZmchsrqSkZhPdmZaex7oXCamuMHs9F");
-pub const ADMINS: [Pubkey; 3] = [
-    pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr5cuzh"),
-    pubkey!("LAdmTEtom7qm3ZmchsrqSkZhPdmZaex7oXCamuMHs9F"),
-    pubkey!("G4QhBg3fF2U7RSwC734ViwL3DeZVrR2TyHMNWHSLwMj"),
-];
+
+// #[cfg(feature = "mainnet")]
+pub const ADMINS: [Pubkey; 1] = [pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr5cuzh")];
+
+// #[cfg(not(feature = "mainnet"))]
+// pub const ADMINS: [Pubkey; 3] = [
+//     pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr5cuzh"),
+//     pubkey!("LAdmTEtom7qm3ZmchsrqSkZhPdmZaex7oXCamuMHs9F"),
+//     pubkey!("G4QhBg3fF2U7RSwC734ViwL3DeZVrR2TyHMNWHSLwMj"),
+// ];
