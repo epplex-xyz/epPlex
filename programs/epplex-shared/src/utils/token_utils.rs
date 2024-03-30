b9e15d70956535dd18882700bb50fb683428437f
Aiyualive <liuaiyu1302@gmail.com>
2024-03-30 16:31:03 +0100

    add lamport extension function

--- programs/epplex-shared/src/utils/token_utils.rs
+++ programs/epplex-shared/src/utils/token_utils.rs
@@ -159,3 +159,22 @@ pub fn transfer_token_with_pda<'info>(
 
     Ok(())
 }
+
+pub fn update_account_lamports_to_minimum_balance<'info>(
+    account: AccountInfo<'info>,
+    payer: AccountInfo<'info>,
+    system_program: AccountInfo<'info>,
+) -> Result<()> {
+    let extra_lamports = Rent::get()?.minimum_balance(account.data_len()) - account.get_lamports();
+    if extra_lamports > 0 {
+        anchor_lang::solana_program::program::invoke(
+            &anchor_lang::solana_program::system_instruction::transfer(
+                payer.key,
+                account.key,
+                extra_lamports,
+            ),
+            &[payer, account, system_program],
+        )?;
+    }
+    Ok(())
+}
