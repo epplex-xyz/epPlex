3032b2b348c7952498b6e3824e65a215ecb5541f
Aiyualive <liuaiyu1302@gmail.com>
2024-04-03 09:50:43 +0200

    Update admin.rs

--- programs/epplex-shared/src/utils/admin.rs
+++ programs/epplex-shared/src/utils/admin.rs
@@ -6,15 +6,17 @@ pub const ADMIN_PUBKEY: Pubkey = pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr
 // pub const ADMIN_PUBKEY: Pubkey = pubkey!("LAdmTEtom7qm3ZmchsrqSkZhPdmZaex7oXCamuMHs9F");
 
 // #[cfg(feature = "mainnet")]
-// pub const ADMINS: [Pubkey; 2] = [
+// pub const ADMINS: [Pubkey; 3] = [
 //     pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr5cuzh"),
 //     pubkey!("MA1NqUiWSgJz4VDXjPFfNoDWqBBRpMDnT4vxEnt9qbv"),
+//     pubkey!("gameAbxppNoqMi4fHHVbQuj9RoYLksh1kX41EJQHTkd"),
 // ];
 
 // #[cfg(not(feature = "mainnet"))]
-pub const ADMINS: [Pubkey; 4] = [
+pub const ADMINS: [Pubkey; 5] = [
     pubkey!("epADzKVW5kb3hjUhKuxdmyASNKYt4Cb1ccLGvr5cuzh"),
     pubkey!("LAdmTEtom7qm3ZmchsrqSkZhPdmZaex7oXCamuMHs9F"),
     pubkey!("G4QhBg3fF2U7RSwC734ViwL3DeZVrR2TyHMNWHSLwMj"),
     pubkey!("MA1NqUiWSgJz4VDXjPFfNoDWqBBRpMDnT4vxEnt9qbv"),
+    pubkey!("gameAbxppNoqMi4fHHVbQuj9RoYLksh1kX41EJQHTkd"),
 ];
