c16e5c5c5cce126fbd34c658d75e2995be69d623
Aiyualive <liuaiyu1302@gmail.com>
2024-06-06 11:31:57 +0200

    Update game_config.rs

--- programs/epplex-burger/src/state/game_config.rs
+++ programs/epplex-burger/src/state/game_config.rs
@@ -313,7 +313,7 @@ impl GameConfig {
 
     pub fn check_valid_collection(
         &self,
-        group_member: &Account<'_, TokenGroupMember>,
+        group_member: &Account<'_, TokenGroupMember2>,
         mint: Pubkey,
     ) -> Result<()> {
         if mint != group_member.mint {
